5385ad3711a2e9fcb0b62fc51505ff3bddf15af4
Prajwal-PL <prajwalpl096@gmail.com>
2024-02-03 11:45:54 +0530

    first challenge

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+app.get('/', (req, res)=>{
+    console.log("Hello World")
+})
+
+app.listen(3500, ()=> console.log("Hello World"))
+
 
 
 
